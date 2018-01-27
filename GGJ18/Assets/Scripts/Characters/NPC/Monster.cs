cc3b6c8597100499e8b67f713ac61c66be927cf5
pvallsten <petval-0@hotmail.com>
2018-01-27 14:29:07 +0100

    Initial commit for navmesh ai

---
+++ GGJ18/Assets/Scripts/Characters/NPC/Monster.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Assets.Scripts.Characters.NPC
+{
+    public class Monster : NonPlayerCharacter
+    {
+        private NavMeshAgent agent;
+
+        private int currentTarget;
+        private int previousTarget;
+
+        private GameObject[] checkpointList;
+
+        // Use this for initialization
+        void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            checkpointList = GameObject.FindGameObjectsWithTag("Waypoint");
+            previousTarget = 0;
+            currentTarget = GetClosestWaypoint();
+            var nextPath = (checkpointList[currentTarget].GetComponent("Waypoint") as Waypoint).GetNextPath(checkpointList[previousTarget].transform);
+            agent.SetDestination(nextPath.transform.position);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (agent.remainingDistance < 0.25f)
+            {
+                var nextPath = (checkpointList[currentTarget].GetComponent("Waypoint") as Waypoint).GetNextPath(checkpointList[previousTarget].transform);
+                agent.SetDestination(nextPath.transform.position);
+                previousTarget = currentTarget;
+                currentTarget = FindIndexInList(nextPath);
+            }
+        }
+
+        int GetClosestWaypoint()
+        {
+            float distance = Mathf.Infinity;
+            var closest = 0;
+            for(int i=0; i<checkpointList.Length; i++)
+            {
+                var diff = checkpointList[i].transform.position - this.transform.position;
+                var currentDistance = diff.sqrMagnitude;
+                if(currentDistance < distance)
+                {
+                    closest = i;
+                    distance = currentDistance;
+                }
+            }
+
+            return closest;
+        }
+
+        int FindIndexInList(GameObject next)
+        {
+            for (int i = 0; i < checkpointList.Length; i++)
+            {
+                if (checkpointList[i] == next)
+                {
+                    return i;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ GGJ18/Assets/Scripts/Characters/NPC/Waypoint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+
+    public Transform[] possiblePaths;
+    public static Color color;
+
+    void OnDrawGizmosSelected()
+    {        
+        Gizmos.color = Color.blue;
+        var addY = new Vector3(0, 1, 0);
+        Gizmos.DrawSphere(transform.position+addY, 0.2f);
+        for (int i = 0; i < possiblePaths.Length; i++)
+        {
+            Gizmos.DrawLine(transform.position + addY, possiblePaths[i].position + addY);
+        }
+    }
+
+    public GameObject GetNextPath(Transform trans)
+    {
+        if(possiblePaths.Length == 1)
+        {
+            return possiblePaths[0].gameObject;
+        }
+
+        var validChoices = new List<int>();
+        for(int i=0; i< possiblePaths.Length;i++)
+        {
+            if(possiblePaths[i] != trans)
+            {
+                validChoices.Add(i);
+            }
+        }
+        var index = validChoices[Random.Range(0, validChoices.Count)];
+        
+        return possiblePaths[index].gameObject;
+    }
+}
