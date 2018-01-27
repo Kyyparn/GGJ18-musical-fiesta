3f7e72d9e21ae13d0fbff688d37c49e29258b037
Simon Lundmark <simon.j.lundmark@gmail.com>
2018-01-27 23:40:49 +0100

    Added component to use on triggers to load a new scene.

---
+++ GGJ18/Assets/Scripts/SceneLoad/SceneLoadTrigger.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Characters.Player;
+using Assets.Scripts.Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.SceneLoad
+{
+    public class SceneLoadTrigger : MonoBehaviour {
+
+        public string sceneName;
+
+        void OnTriggerEnter(Collider other)
+        {
+            var player = other.gameObject.GetComponent<Player>();
+
+            if(player)
+            {
+                print("Collided");
+                SceneLoadManager.Instance.LoadScene(sceneName);
+            }
+        }
+    }
+}
