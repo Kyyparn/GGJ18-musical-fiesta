ee53ff30b5ec083149af49db0cf96ab2ae420ae5
randcorn <daniel.lundberg@byske.com>
2018-01-27 11:59:18 +0100

    Adding the base for the sonar shader

---
+++ GGJ18/Assets/Scripts/Echos/EchoControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class EchoControl : MonoBehaviour {
+
+    public int intensity;
+
+    private SonarShader sonarShader;
+
+    // Use this for initialization
+    void Start () {
+        sonarShader = GetComponent<SonarShader>();
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            sonarShader.StartSonarRing(transform.position, intensity);
+        }
+        // Start sonar ring from the contact point
+    }
+}
