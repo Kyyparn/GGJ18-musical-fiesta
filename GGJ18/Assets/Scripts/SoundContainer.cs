d818d650ae02dc2b1879ac90f790c67b63c1431a
pvallsten <petval-0@hotmail.com>
2018-01-27 20:53:19 +0100

    Added Scriptableobject and initial commit for monster sounds

---
+++ GGJ18/Assets/Scripts/SoundContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class SoundContainer : ScriptableObject
+{
+    public List<AudioClip> ListOfSounds;
+}
