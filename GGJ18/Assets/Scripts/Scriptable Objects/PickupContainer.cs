8d2eec84eb0be9be9eff05058b6f945df22b607b
randcorn <daniel.lundberg@byske.com>
2018-01-28 12:21:22 +0100

    Adding prefabs for bottles and they now make sounds

--- GGJ18/Assets/Scripts/Scriptable Objects/PickupContainer.cs
+++ GGJ18/Assets/Scripts/Scriptable Objects/PickupContainer.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu()]
 public class PickupContainer : ScriptableObject
 {
     public List<AudioClip> ThrowSound;
+    public float throwSoundIntensity;
     public List<AudioClip> PickupSound;
+    public float pickUpSoundIntensity;
 }
 
