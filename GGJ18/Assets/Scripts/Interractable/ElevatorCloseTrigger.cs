f3a7ee60f5819e4ddba5e7d6b9a948b150ad455f
Simon Lundmark <simon.j.lundmark@gmail.com>
2018-01-28 14:17:16 +0100

    Added a close trigger scripts for elevator. Added a close trigger.

---
+++ GGJ18/Assets/Scripts/Interractable/ElevatorCloseTrigger.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Characters.Player;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorCloseTrigger : MonoBehaviour {
+
+    public Elevator elevator;
+
+    void OnTriggerEnter(Collider other)
+    {
+        var player = other.gameObject.GetComponent<Player>();
+
+        if (player)
+        {
+            elevator.CloseDoors();
+        }
+    }
+}
