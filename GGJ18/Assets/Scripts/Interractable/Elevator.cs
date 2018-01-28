2af04ec35b511920550cdce6caa4b507cbc29807
randcorn <daniel.lundberg@byske.com>
2018-01-28 13:03:59 +0100

    Adding elevator prefab

---
+++ GGJ18/Assets/Scripts/Interractable/Elevator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public bool IsElevatorOpen;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        if(IsElevatorOpen)
+        {
+            animator.SetBool("OpenDoor", true);
+        }
+        else
+        {
+            animator.SetBool("CloseDoor", true);
+        }
+    }
+
+    public void OpenDoors()
+    {
+        if (! IsElevatorOpen)
+        {
+            animator.SetBool("CloseDoor", false);
+            animator.SetBool("OpenDoor", true);
+            IsElevatorOpen = true;
+        }
+    }
+
+    public void CloseDoors()
+    {
+        if (IsElevatorOpen)
+        {
+            animator.SetBool("CloseDoor", true);
+            animator.SetBool("OpenDoor", false);
+            IsElevatorOpen = false;
+        }
+    }
+}
---
+++ GGJ18/Assets/Scripts/Interractable/ElevatorButton.cs
@@ -0,0 +1,13 @@
+﻿public class ElevatorButton : Interactable {
+
+    public Elevator Elevator;
+
+    // Use this for initialization
+    public override void Interract()
+    {
+        if(Elevator.IsElevatorOpen)
+        {
+            Elevator.CloseDoors();
+        }
+    }
+}
