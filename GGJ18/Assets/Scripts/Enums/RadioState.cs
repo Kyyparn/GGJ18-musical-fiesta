23b19b27f91f3cf9fc55f8115f8bb0f20a38dde3
randcorn <daniel.lundberg@byske.com>
2018-01-28 11:55:43 +0100

    Fixing the scene with collisions and material

---
+++ GGJ18/Assets/Scripts/Enums/RadioState.cs
@@ -0,0 +1,5 @@
+﻿public enum RadioState
+{
+    Static,
+    Churchill
+}
---
+++ GGJ18/Assets/Scripts/Radio.cs
@@ -0,0 +1,58 @@
+﻿using Assets.Scripts.Echoes;
+using UnityEngine;
+
+public class Radio : Interactable
+{
+    public AudioClip StaticRadio;
+    public float StaticRadioIntensity;
+    public float StaticRadioVolume;
+    public AudioClip ChurchillSpeech;
+    public float ChurchillSpeechIntensity;
+    public float ChurchillSpeechVolume;
+
+    private RadioState state;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(!audioSource.isPlaying)
+        {
+            SwitchToStatic();
+        }
+    }
+
+    public override void Interract()
+    {
+        if (state == RadioState.Static)
+        {
+            SwitchToChurchill();
+        }
+    }
+
+    public void SwitchToChurchill()
+    {
+        audioSource.clip = ChurchillSpeech;
+        audioSource.loop = false;
+        audioSource.volume = ChurchillSpeechVolume;
+        var echoAudio = GetComponent<EcoLocationAudioSource>();
+        echoAudio.intensity = ChurchillSpeechIntensity;
+        echoAudio.isAmbientSound = false;
+        audioSource.Play();
+    }
+
+    public void SwitchToStatic()
+    {
+        audioSource.clip = StaticRadio;
+        audioSource.loop = true;
+        audioSource.volume = StaticRadioVolume;
+        var echoAudio = GetComponent<EcoLocationAudioSource>();
+        echoAudio.intensity = StaticRadioIntensity;
+        echoAudio.isAmbientSound = true;
+        audioSource.Play();
+    }
+}
